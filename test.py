eedc0782b2e284a86a438e992701875f599065e5
wangbar0133 <1536166036@qq.com>
2022-10-06 11:15:14 +0800

    add intoverflow solc version check

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestScanner(unittest.TestCase):
     def test_ast_scanner(self):
         from solidity_antlr4_parser.parser import parse_file, objectify
         from ast_scanner.ast_scanner import Scanner
-        path = "/Users/wang/dev/Asuka/test1.sol"
+        path = "/Users/wang/dev/Asuka/test_data/test.sol"
         source_unit = parse_file(path, loc=True)
         source_unit_object = objectify(source_unit, path)
         scanner = Scanner(source_unit_object)
@@ -23,6 +23,16 @@ class TestVuls(unittest.TestCase):
         scanner = Scanner(source_unit_object)
         vuls = check(scanner)
         pass
+    
+    def test_check_solc_version(self):
+        from vuls.int_overflow import check_solc_version
+        self.assertEqual(True, check_solc_version("<=0.8.0", 8))
+        self.assertEqual(False, check_solc_version("^0.8.0", 8))
+        self.assertEqual(False, check_solc_version("0.8.0", 8))
+        self.assertEqual(True, check_solc_version("<0.8.0", 8))
+        self.assertEqual(True, check_solc_version("<0.7.0", 8))
+        self.assertEqual(True, check_solc_version("^0.7.0", 8))
+        self.assertEqual(True, check_solc_version("0.4.0", 8))
         
     def test_function_default_visibility(self):
         from solidity_antlr4_parser.parser import parse_file, objectify
--- vuls/int_overflow.py
+++ vuls/int_overflow.py
@@ -15,15 +15,18 @@ RISK = 3
 
 def check(scan: Scanner):
     vul_list = list()
-    if scan.operators:
-        for op in scan.operators:
-            if op.name in INVAILD_OP:
-                vul_list.append(
-                    Vul(
-                        fileName=scan.fileName,
-                        locList=[op["loc"]]
-                    )
-                )
+    for pragma in scan.sourceUnitObject.pragmas:
+        if check_solc_version(pragma.value, 8):
+            if scan.operators:
+                for op in scan.operators:
+                    if op.name in INVAILD_OP:
+                        vul_list.append(
+                            Vul(
+                                fileName=scan.fileName,
+                                locList=[op["loc"]]
+                            )
+                        )
+            break
                 
     return Vuls(
             name=NAME,
@@ -33,3 +36,21 @@ def check(scan: Scanner):
             risk=RISK,
             vulList=vul_list
         )
+    
+
+def check_solc_version(pragma: str, version: int):
+    try:
+        major, minor, patch = tuple(pragma.split("."))
+        prefix = major[:-1]
+        if prefix in ["<", "<="]:
+            return True
+        if int(minor) < version:
+            return True
+        return False
+    except:
+        return False
+
+
+
+
+
